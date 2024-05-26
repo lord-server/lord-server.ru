abd09f4f3ff4dab2895eac8f92ed75fedc76bd09
Alexander Chibrikin <alek13.me@gmail.com>
2024-05-26 17:58:48 +0300

    Add migration for `clans` table. Relates to #24

---
+++ database/migrations/2024_05_25_000001_create_clans_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 32);
+            $table->string('title', 64);
+            $table->string('about', 350)->nullable();
+            $table->text('description')->nullable();
+            $table->foreignId('leader_id')->constrained('players');
+            $table->foreignId('negotiator_id')->constrained('players');
+            $table->timestamps();
+        });
+
+        Schema::table('players', function (Blueprint $table) {
+            $table->foreignId('clan_id')->after('last_login')->nullable()->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('players', function (Blueprint $table) {
+            $table->dropForeign('players_clan_id_foreign');
+        });
+        Schema::dropColumns('players', 'clan_id');
+        Schema::drop('clans');
+    }
+};
