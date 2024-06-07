148fbc318d4f64932d52fb155f5f8796a4be0a82
Alexander Chibrikin <alek13.me@gmail.com>
2024-06-07 23:13:57 +0300

    Handle `500` error after user cancel grant access on Discord side. Closes #37

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Controller;
 use App\Models\User;
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
 use SocialiteProviders\Manager\OAuth2\User as DiscordUser;
+use Throwable;
 
 class AuthController extends Controller
 {
@@ -29,29 +31,44 @@ class AuthController extends Controller
      */
     public function comeback(): RedirectResponse
     {
-        /** @var DiscordUser $discordUser */
-        $discordUser = Socialite::driver('discord')->user();
+        try {
+            /** @var DiscordUser $discordUser */
+            $discordUser = Socialite::driver('discord')->user();
 
-        $user = User::findByDiscordId($discordUser->id);
-        if ( ! $user) {
-            $user = User::create([
-                'name'                  => $discordUser->name,
-                'email'                 => $discordUser->email,
-                'discord_id'            => $discordUser->id,
-                'discord_token'         => $discordUser->token,
-                'discord_refresh_token' => $discordUser->refreshToken,
-                'discord_avatar'        => $discordUser->avatar,
-                'password'              => '_generated:' . Str::random(),
-            ]);
-        } else {
-            $user->update([
-                'discord_token'         => $discordUser->token,
-                'discord_refresh_token' => $discordUser->refreshToken,
-                'discord_avatar'        => $discordUser->avatar,
-            ]);
+            $user = User::findByDiscordId($discordUser->id);
+            if ( ! $user) {
+                $user = User::create([
+                    'name'                  => $discordUser->name,
+                    'email'                 => $discordUser->email,
+                    'discord_id'            => $discordUser->id,
+                    'discord_token'         => $discordUser->token,
+                    'discord_refresh_token' => $discordUser->refreshToken,
+                    'discord_avatar'        => $discordUser->avatar,
+                    'password'              => '_generated:' . Str::random(),
+                ]);
+            } else {
+                $user->update([
+                    'discord_token'         => $discordUser->token,
+                    'discord_refresh_token' => $discordUser->refreshToken,
+                    'discord_avatar'        => $discordUser->avatar,
+                ]);
+            }
+
+            Auth::login($user);
+
+        } catch (ClientException) {
+            return redirect('/')->with(
+                'warning',
+                __('messages.Failed to login') . ': ' . __('messages.Can\'t get Discord token')
+            );
+        } catch (Throwable $exception) {
+            report($exception);
+
+            return redirect('')->with(
+                'error',
+                __('messages.Failed to login') . ': ' . __('messages.Internal server error')
+            );
         }
-
-        Auth::login($user);
 
         return redirect('/');
     }
---
+++ lang/en/messages.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    //--------------------------------------------------------------------------
+    // Alerts prefixes (Layout)
+    //--------------------------------------------------------------------------
+    'Well done!'               => 'Well done!',
+    'Warning!'                 => 'Warning!',
+    'Info:'                    => 'Info:',
+    'Error!'                   => 'Error!',
+    //--------------------------------------------------------------------------
+    // Alerts messages
+    //--------------------------------------------------------------------------
+    'Failed to login'          => 'Failed to login',
+    'Can\'t get Discord token' => 'Can\'t get Discord token',
+    'Internal server error'    => 'Internal server error',
+];
